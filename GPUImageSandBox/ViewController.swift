a6de9534680b477bd3d635bc84d82a472506487b
m0a <m0a@m0a-mac.local>
2014-12-21 17:48:14 +0900

    add GPUImage

--- GPUImageSandBox/ViewController.swift
+++ GPUImageSandBox/ViewController.swift
@@ -7,13 +7,24 @@
 //
 
 import Cocoa
+import GPUImage
+
 
 class ViewController: NSViewController {
 
+    var videoCamera: GPUImageAVCamera?
+    
+    @IBOutlet var screenView: GPUImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        videoCamera = GPUImageAVCamera(sessionPreset: AVCaptureSessionPreset1280x720, cameraDevice:nil)
 
         // Do any additional setup after loading the view.
+        videoCamera?.addTarget(screenView!)
+        
+        videoCamera!.startCameraCapture()
+
+        
     }
 
     override var representedObject: AnyObject? {
