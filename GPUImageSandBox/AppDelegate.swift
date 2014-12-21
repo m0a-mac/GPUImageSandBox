fc09699258f47c4f407e745bf409986a09435e35
m0a <m0a@m0a-mac.local>
2014-12-21 17:30:41 +0900

    Initial Commit

---
+++ GPUImageSandBox/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GPUImageSandBox
+//
+//  Created by m0a on 2014/12/21.
+//  Copyright (c) 2014年 m0a. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
